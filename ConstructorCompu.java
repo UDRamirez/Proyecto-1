ebc5c69950e54c8d904de6e4181da9974a3816cd
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-19 17:53:33 -0600

    Create ConstructorCompu.java

---
+++ ConstructorCompu.java
@@ -0,0 +1,18 @@
+public interface ConstructorCompu{
+  
+    void agregarCPU(CPU cpu);
+  
+    void agregarGPU(GPU gpu);
+  
+    void agregarMadre(Madre madre);
+  
+    void agregarRAM(RAM ram);
+  
+    void agregarDisco(Disco disco);
+  
+    void agregarFuente(Fuente fuente);
+  
+    void agregarGabinete(Gabinete gab);
+
+    PC build();
+}
