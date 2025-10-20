4edcf1db1bd2da94459e9456183ff9872aea2c33
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-20 17:12:00 -0600

    Create CompuABuilder.java

---
+++ CompuABuilder.java
@@ -0,0 +1,47 @@
+public class CompuABuilder implements ConstructorCompu {
+    private PC pc;
+
+    public CompuABuilder() {
+        this.pc = new PC(); 
+    }
+
+    @Override
+    public void agregarCPU(CPU cpu){ 
+        pc.setCPU(cpu); 
+    }
+
+    @Override
+    public void agregarGPU(GPU gpu){
+        pc.setGPU(gpu); 
+    }
+
+    @Override
+    public void agregarMadre(Madre madre){ 
+        pc.setMadre(madre); 
+    }
+
+    @Override
+    public void agregarRAM(RAM ram){
+        pc.addRAM(ram);
+    }
+
+    @Override
+    public void agregarDisco(Disco disco){ 
+        pc.addDisco(disco);
+    }
+
+    @Override
+    public void agregarFuente(Fuente fuente){ 
+        pc.setFuente(fuente); 
+    }
+
+    @Override
+    public void agregarGabinete(Gabinete gabinete){
+        pc.setGabinete(gabinete); 
+    }
+
+    @Override
+    public PC build() {
+        return pc;
+    }
+}
