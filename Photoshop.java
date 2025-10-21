187455378d4fbc52bad47e1c2edc9bbef2974b85
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-20 19:10:16 -0600

    Create Photoshop.java

---
+++ Photoshop.java
@@ -0,0 +1,7 @@
+public class Photoshop extends Software {
+  
+    public Photoshop(String version, double precio) {
+        super("Adobe Photoshop " + version, "Precio: " + precio);
+      
+    }
+}
