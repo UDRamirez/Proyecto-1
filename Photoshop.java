518a22e3462418281c1a449ab59ddb7937cac2ec
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-20 19:10:33 -0600

    Update Photoshop.java

--- Photoshop.java
+++ Photoshop.java
@@ -1,4 +1,4 @@
-public class Photoshop extends Software {
+public class Photoshop extends Programa {
   
     public Photoshop(String version, double precio) {
         super("Adobe Photoshop " + version, "Precio: " + precio);
