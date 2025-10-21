2aa885c204bc1c86fca1d33f9bec1aa29230891c
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-21 16:16:56 -0600

    Update Gabinete.java

--- Gabinete.java
+++ Gabinete.java
@@ -3,4 +3,10 @@ public class Gabinete extends Pieza {
     public Gabinete(String nombre, String marca, double precio) {
         super(nombre, marca, precio, "Gabinete");
     }
+
+  @Override
+    public String toString() {
+        return super.toString();
+    }
+  
 }
