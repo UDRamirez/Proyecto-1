3975bb02111708cf5e01fb631f9769c38b277fb3
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-21 16:15:14 -0600

    Update Fuente.java

--- Fuente.java
+++ Fuente.java
@@ -9,4 +9,9 @@ public class Fuente extends Pieza {
     public int getPotencia(){ 
       return potencia;
     }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Potencia: " + potencia + " W";
+    }
 }
