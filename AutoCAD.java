afc855249281dfaeccb14a39f7e688a02e90eab1
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-23 22:19:15 -0600

    Update AutoCAD.java

--- AutoCAD.java
+++ AutoCAD.java
@@ -1,7 +1,20 @@
+/**
+ * Clase {@code AutoCAD} que extiende {@link Programa}.
+ * <p>
+ * Representa una versión específica del software de diseño asistido por computadora AutoCAD.
+ * Cada instancia incluye la versión del programa y su precio.
+ * </p>
+ */
 public class AutoCAD extends Programa {
-  
+
+    /**
+     * Constructor que crea una instancia de {@code AutoCAD} con la versión y precio especificados.
+     *
+     * @param version la versión del software AutoCAD (por ejemplo, "2024").
+     * @param precio el precio del programa.
+     */
     public AutoCAD(String version, double precio) {
         super("AutoCAD " + version, precio);
     }
-  
+
 }
