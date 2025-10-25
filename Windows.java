f61341a24b23961840c85dac6284055f37ac5a71
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-24 20:03:10 -0600

    Update Windows.java

--- Windows.java
+++ Windows.java
@@ -1,7 +1,18 @@
+/**
+ * Clase {@code Windows}.
+ * <p>
+ * Representa el sistema operativo Windows como un tipo de {@link Programa}.
+ * </p>
+ */
 public class Windows extends Programa {
-  
+
+    /**
+     * Crea una nueva instancia de {@code Windows}.
+     *
+     * @param version versión del sistema operativo (por ejemplo, "10").
+     * @param precio  precio del sistema operativo.
+     */
     public Windows(String version, double precio) {
         super("Windows 10", 500.0);
-      
     }
 }
