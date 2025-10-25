6caf6f910d465cf0f4677f6365c9c754c7c7b648
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-24 19:57:05 -0600

    Update Office.java

--- Office.java
+++ Office.java
@@ -1,5 +1,17 @@
+/**
+ * Clase {@code Office}.
+ * <p>
+ * Representa el programa Microsoft Office como un tipo de {@link Programa}.
+ * </p>
+ */
 public class Office extends Programa {
-  
+
+    /**
+     * Crea una nueva instancia de {@code Office}.
+     *
+     * @param version versión del programa (por ejemplo, "2024").
+     * @param precio  precio del programa.
+     */
     public Office(String version, double precio) {
         super("Microsoft Office 2024 ", 500.0);
     }
