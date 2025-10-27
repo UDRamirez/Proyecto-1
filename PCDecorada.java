fe0f783d213fbd72c1fbee643c6951818674701e
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-26 22:05:01 -0600

    Update PCDecorada.java

--- PCDecorada.java
+++ PCDecorada.java
@@ -4,51 +4,55 @@ import java.util.List;
 public class PCDecorada implements Compunent {
 
     private Compunent compu;
-    private Programa programa;
-    private List<String> instalados;
+    private List<Programa> instalados;
 
     public PCDecorada(Compunent compu, Programa programa) {
         this.compu = compu;
-        this.programa = programa;
         this.instalados = new ArrayList<>();
 
+        // Si el componente ya es una PCDecorada, copia sus programas instalados
         if (compu instanceof PCDecorada) {
             this.instalados.addAll(((PCDecorada) compu).getProgramas());
         }
 
-        if (!instalados.contains(programa.getNombre())) {
-            this.instalados.add(programa.getNombre());
+        // Evita duplicados
+        if (!tienePrograma(programa.getNombre())) {
+            this.instalados.add(programa);
         } else {
             System.out.println("Advertencia: El programa '" + programa.getNombre() + "' ya está instalado y no se agregará nuevamente.");
         }
     }
 
-    public List<String> getProgramas() {
+    public List<Programa> getProgramas() {
         return instalados;
     }
 
     public boolean tienePrograma(String nombre) {
-        return instalados.contains(nombre);
+        for (Programa p : instalados) {
+            if (p.getNombre().equals(nombre)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public double getPrecio() {
-        return compu.getPrecio() + (instalados.contains(programa.getNombre()) ? programa.getPrecio() : 0);
+        double precioTotal = compu.getPrecio();
+        for (Programa p : instalados) {
+            precioTotal += p.getPrecio();
+        }
+        return precioTotal;
     }
 
-   @Override
-public String getDescripcion() {
-    StringBuilder desc = new StringBuilder(compu.getDescripcion());
-
-    desc.append("\n\nProgramas instalados:");
-
-    // Evita duplicados y muestra todos los nombres
-    for (String p : instalados) {
-        desc.append("\n  + ").append(p);
+    @Override
+    public String getDescripcion() {
+        StringBuilder desc = new StringBuilder(compu.getDescripcion());
+        desc.append("\n\nProgramas instalados:");
+        for (Programa p : instalados) {
+            desc.append("\n + ").append(p.getNombre());
+        }
+        desc.append("\nPrecio total: $").append(getPrecio());
+        return desc.toString();
     }
-
-    desc.append("\nPrecio total: $").append(getPrecio());
-
-    return desc.toString();
-}
 }
