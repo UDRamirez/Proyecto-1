59bb5f80d6010bc5eb542e702da8fca244e5f50c
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-27 18:59:13 -0600

    Update Compu.java

--- Compu.java
+++ Compu.java
@@ -67,7 +67,7 @@ public class Compu implements Compunent {
     @Override
     public String getDescripcionBase() {
         StringBuilder sb = new StringBuilder();
-        sb.append("===== Detalles de tu nueva PC =====\n");
+        sb.append("===== Detalles esta PC =====\n");
 
         sb.append("CPU: ").append(cpu != null ? cpu.getNombre() : "No seleccionado");
         if (cpu != null && cpu.getAdaptado()) sb.append(" [ADAPTADO]");
