abb85176169996cc12586b368f04c63645d52d0c
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-27 23:23:30 -0600

    Update FabricaCPU.java

--- FabricaCPU.java
+++ FabricaCPU.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  * Clase {@code FabricaCPU}.
  * <p>
  * Implementa la interfaz {@link ContratoFabrica} y se encarga de crear instancias
  * de procesadores {@link CPU} específicos según el modelo solicitado.
+ * Esta clase forma parte del patrón <b>Abstract Factory</b>, permitiendo
+ * la creación de CPUs de distintas marcas (Intel o AMD) sin acoplar el código
+ * a las clases concretas.
  * </p>
  */
 public class FabricaCPU implements ContratoFabrica {
@@ -23,6 +27,7 @@ public class FabricaCPU implements ContratoFabrica {
     @Override
     public Pieza crearComponente(String modelo) {
         switch (modelo.toLowerCase()) {
+            //Intel
             case "core i3-13100":
                 return new CPU("Core i3-13100", "Intel", 1500, 4);
             case "core i5-13600k":
@@ -31,26 +36,41 @@ public class FabricaCPU implements ContratoFabrica {
                 return new CPU("Core i7-13700K", "Intel", 5800, 8);
             case "core i9-13900k":
                 return new CPU("Core i9-13900K", "Intel", 9500, 16);
+
+            //AMD
+            case "ryzen 5 5600g":
+                return new CPU("Ryzen 5 5600G", "AMD", 1500, 4);
+            case "ryzen 5 7600x":
+                return new CPU("Ryzen 5 7600X", "AMD", 3000, 6);
+            case "ryzen 7 7700x":
+                return new CPU("Ryzen 7 7700X", "AMD", 6000, 8);
+            case "ryzen 9 7950x3d":
+                return new CPU("Ryzen 9 7950X3D", "AMD", 9000, 16);
+
             default:
-                System.out.println("No se ha encontrado este modelo: " + modelo);
+                System.out.println(" No se ha encontrado este modelo de CPU");
                 return null;
         }
     }
 
-     @Override
+    /**
+     * Retorna la lista de modelos de CPU disponibles en esta fábrica.
+     *
+     * @return una lista con los nombres de los modelos de CPU.
+     */
+    @Override
     public List<String> getModelos() {
         List<String> modelos = new ArrayList<>();
+        // Intel
         modelos.add("Core i3-13100");
         modelos.add("Core i5-13600K");
         modelos.add("Core i7-13700K");
-        modelos.add("Core i3-13100");
         modelos.add("Core i9-13900K");
+        // AMD
+        modelos.add("Ryzen 5 5600G");
+        modelos.add("Ryzen 5 7600X");
+        modelos.add("Ryzen 7 7700X");
+        modelos.add("Ryzen 9 7950X3D");
         return modelos;
-        
     }
 }
-
-
-
-
-
