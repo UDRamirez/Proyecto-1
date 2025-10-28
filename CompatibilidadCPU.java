3fa87d5578de8bdc103464b2695034f4abad781a
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-28 13:51:49 -0600

    Update CompatibilidadCPU.java

--- CompatibilidadCPU.java
+++ CompatibilidadCPU.java
@@ -7,6 +7,23 @@ import java.util.*;
  * utilizando el patrón Adaptador. También permite obtener una lista de piezas
  * compatibles a partir de una pieza de referencia.
  * </p>
+ *
+ * <h2>Funcionamiento:</h2>
+ * <ul>
+ *   <li>{@link #esCompatible(AdaptadorPieza, AdaptadorPieza)}: Comprueba si dos piezas adaptadas son compatibles.</li>
+ *   <li>{@link #getPiezasAlternativas(AdaptadorPieza, List)}: Filtra una lista de piezas candidatas, devolviendo solo las compatibles con la pieza de referencia.</li>
+ * </ul>
+ *
+ * <h2>Ejemplo de uso:</h2>
+ * <pre>{@code
+ * AdaptadorCPU cpu = new AdaptadorCPU(new CPU("Ryzen 5 5600X", "AMD", 250.0, 6));
+ * AdaptadorMadre madre = new AdaptadorMadre(new Madre("Asus B550", "AMD", 150.0, "AMD"));
+ * CompatibilidadCPU compat = new CompatibilidadCPU();
+ * boolean compatible = compat.esCompatible(cpu, madre);
+ * }</pre>
+ *
+ * @see AdaptadorPieza
+ * @version 1.0
  */
 public class CompatibilidadCPU {
 
