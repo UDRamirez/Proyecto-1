5c87c959ffecc025a911ca55c338f7842a338dcb
Sensationalforge <mikeforge@ciencias.unam.mx>
2025-10-28 17:22:05 -0600

    Update ArmadaBuilder.java

--- ArmadaBuilder.java
+++ ArmadaBuilder.java
@@ -1,18 +1,18 @@
 /**
  * Clase {@code ArmadaBuilder} que implementa la interfaz {@link ConstructorCompu}.
  * <p>
- * Esta clase concreta del patrón <b>Builder</b> se encarga de construir objetos
+ * Esta clase concreta del patron <b>Builder</b> se encarga de construir objetos
  * del tipo {@link Compu} paso a paso, agregando sus distintos componentes de hardware.
  * <br><br>
  * Se utiliza principalmente para crear computadoras ya armadas o predefinidas
- * dentro del sistema, encapsulando el proceso de construcción y evitando que el
- * código cliente deba preocuparse por los detalles internos de la creación.
+ * dentro del sistema, encapsulando el proceso de construccion y evitando que el
+ * codigo cliente deba preocuparse por los detalles internos de la creacion.
  * </p>
  *
- * <h2>Patrón de diseño aplicado:</h2>
+ * <h2>Patron de diseno aplicado:</h2>
  * <ul>
  *   <li><b>Builder</b>: Permite construir objetos complejos (como una PC)
- *       mediante pasos controlados, separando la construcción de su representación.</li>
+ *       mediante pasos controlados, separando la construccion de su representacion.</li>
  * </ul>
  *
  * @version 1.0
@@ -20,13 +20,13 @@
 public class ArmadaBuilder implements ConstructorCompu {
 
     /**
-     * Instancia de la computadora que se está construyendo.
+     * Instancia de la computadora que se esta construyendo.
      */
     private Compu pc;
 
     /**
-     * Constructor que inicializa un nuevo objeto {@link Compu} vacío,
-     * listo para comenzar el proceso de construcción.
+     * Constructor que inicializa un nuevo objeto {@link Compu} vacio,
+     * listo para comenzar el proceso de construccion.
      */
     public ArmadaBuilder() {
         this.pc = new Compu();
@@ -35,7 +35,7 @@ public class ArmadaBuilder implements ConstructorCompu {
     /**
      * Agrega un componente de tipo {@link CPU} a la computadora.
      *
-     * @param cpu el procesador que se agregará.
+     * @param cpu el procesador que se agregara.
      */
     @Override
     public void agregarCPU(CPU cpu) {
@@ -43,9 +43,9 @@ public class ArmadaBuilder implements ConstructorCompu {
     }
 
     /**
-     * Agrega un componente de tipo {@link GPU} (tarjeta gráfica) a la computadora.
+     * Agrega un componente de tipo {@link GPU} (tarjeta grafica) a la computadora.
      *
-     * @param gpu la tarjeta gráfica que se agregará.
+     * @param gpu la tarjeta grafica que se agregara.
      */
     @Override
     public void agregarGPU(GPU gpu) {
@@ -55,7 +55,7 @@ public class ArmadaBuilder implements ConstructorCompu {
     /**
      * Agrega una placa madre ({@link Madre}) a la computadora.
      *
-     * @param madre la placa madre que se agregará.
+     * @param madre la placa madre que se agregara.
      */
     @Override
     public void agregarMadre(Madre madre) {
@@ -63,9 +63,9 @@ public class ArmadaBuilder implements ConstructorCompu {
     }
 
     /**
-     * Agrega un módulo de memoria RAM ({@link RAM}) a la computadora.
+     * Agrega un modulo de memoria RAM ({@link RAM}) a la computadora.
      *
-     * @param ram la memoria RAM que se agregará.
+     * @param ram la memoria RAM que se agregara.
      */
     @Override
     public void agregarRAM(RAM ram) {
@@ -75,7 +75,7 @@ public class ArmadaBuilder implements ConstructorCompu {
     /**
      * Agrega un dispositivo de almacenamiento ({@link Disco}) a la computadora.
      *
-     * @param disco el disco duro o SSD que se agregará.
+     * @param disco el disco duro o SSD que se agregara.
      */
     @Override
     public void agregarDisco(Disco disco) {
@@ -85,7 +85,7 @@ public class ArmadaBuilder implements ConstructorCompu {
     /**
      * Agrega una fuente de poder ({@link Fuente}) a la computadora.
      *
-     * @param fuente la fuente de poder que se agregará.
+     * @param fuente la fuente de poder que se agregara.
      */
     @Override
     public void agregarFuente(Fuente fuente) {
@@ -95,7 +95,7 @@ public class ArmadaBuilder implements ConstructorCompu {
     /**
      * Agrega un gabinete ({@link Gabinete}) a la computadora.
      *
-     * @param gabinete el gabinete que se agregará.
+     * @param gabinete el gabinete que se agregara.
      */
     @Override
     public void agregarGabinete(Gabinete gabinete) {
@@ -104,7 +104,7 @@ public class ArmadaBuilder implements ConstructorCompu {
 
     /**
      * Devuelve la computadora completamente construida con todos los
-     * componentes agregados durante el proceso de construcción.
+     * componentes agregados durante el proceso de construccion.
      *
      * @return el objeto {@link Compu} resultante.
      */
@@ -113,4 +113,3 @@ public class ArmadaBuilder implements ConstructorCompu {
         return pc;
     }
 }
-
